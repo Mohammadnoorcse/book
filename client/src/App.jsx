2814a3c8572142ddcc41b1ca3b6eca36de77d43c
mohammadnoor <mohammadnoorcse@gmail.com>
2024-08-25 13:53:45 +0600

    working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from './page/Layout/Navbar'
+import Footer from './page/Layout/Footer'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './page/Home/Home'
 
 const App = () => {
   return (
-    <div></div>
+    <BrowserRouter>
+     <Navbar />
+      <Routes>
+       <Route path='/' element={<Home/>}/>
+      </Routes>
+     <Footer />
+    </BrowserRouter>
   )
 }
 
