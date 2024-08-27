ffa67e444a63c50d87d7cce94204d309f8b2a027
mohammadnoor <mohammadnoorcse@gmail.com>
2024-08-28 02:54:21 +0600

    working app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,22 @@ import Footer from './page/Layout/Footer'
 import Home from './page/Home/Home'
 import ProtectUser from './page/Protect/ProtectUser'
 import BookDetails from './page/book/BookDetails'
+import Login from './page/LoginAndRegistration/Login'
+import BecomeSeller from './page/LoginAndRegistration/BecomeSeller'
+import Signup from './page/LoginAndRegistration/Signup'
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<ProtectUser element={<BookDetails />} />} />
+        <Route
+          path="/book/:id"
+          element={<ProtectUser element={<BookDetails />} />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/becomeseller" element={<BecomeSeller />} />
       </Routes>
 
       <Footer />
