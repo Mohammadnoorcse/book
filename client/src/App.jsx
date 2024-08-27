b8047d3998755ed82831b94a06ccbf01b450f370
mohammadnoor <mohammadnoorcse@gmail.com>
2024-08-28 02:46:03 +0600

    working navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navbar from './page/Layout/Navbar'
 import Footer from './page/Layout/Footer'
 import Home from './page/Home/Home'
+import ProtectUser from './page/Protect/ProtectUser'
+import BookDetails from './page/book/BookDetails'
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar/>
-       <Routes>
-
-        <Route path='/' element={<Home/>}/>
-       </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book/:id" element={<ProtectUser element={<BookDetails />} />} />
+      </Routes>
 
       <Footer />
     </BrowserRouter>
